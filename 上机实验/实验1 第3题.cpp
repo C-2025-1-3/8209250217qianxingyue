7740a27914294b0e915ccd44be01ead89899dfb8
QianXY <3088336215@qq.com>
2025-11-13 21:30:43 +0800

    250217-钱星月

---
+++ 上机实验/实验1 第3题.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+		cout << "char length=" << sizeof(char) << endl;
+		cout << "int length=" << sizeof(int) << endl;
+		cout << "bool length=" << sizeof(bool) << endl;
+		cout << "float length=" << sizeof(float) << endl;
+		cout << "long length=" << sizeof(long) << endl;
+		cout << "long long  length=" << sizeof(long long) << endl;
+		cout << "short length=" << sizeof(short) << endl;
+
+
+		system("pause");
+
+
+		return 0;
+}
